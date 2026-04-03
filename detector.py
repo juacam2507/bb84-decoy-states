a3506662d729d8d9d128d3389d16163cd033087d
Camilo T <juacam2507@gmail.com>
2026-04-03 11:28:15 -0500

    Added docstrings and typehints to Detector class methods

--- detector.py
+++ detector.py
@@ -3,92 +3,170 @@ from source import Source
 
 
 class Detector:
-    def __init__(self, simulation_parameters: dict, rng):
-        
+    def __init__(self, simulation_parameters: dict, rng: np.random.Generator):
+        """
+        Initialize a photon detector object for the BB84 simulation. The detector is modeled
+        as a two detector model with the respective probability for the 4 possible events.
+
+        Args:
+            simulation_parameters (dict): Dictionary containing the simulation parameters
+            rng (np.random.Generator): Random number generator
+        """
+
         self.N = simulation_parameters["N"]
         self.rng = rng
         self.debug = simulation_parameters["debug"]
-        
+
         self.l = simulation_parameters["channel_properties"]["l"]
         self.beta = simulation_parameters["channel_properties"]["beta"]
-        
+
         self.t_bob = simulation_parameters["detector_properties"]["receiver_transmit"]
         self.eta_d = simulation_parameters["detector_properties"]["detector_efficiency"]
-        self.e_d   = simulation_parameters["detector_properties"]["detector_error"]
-        self.y_0   = simulation_parameters["detector_properties"]["dark_count_rate"]
-        self.e_0   = simulation_parameters["detector_properties"]["dark_count_error"]
-    
+        self.e_d = simulation_parameters["detector_properties"]["detector_error"]
+        self.y_0 = simulation_parameters["detector_properties"]["dark_count_rate"]
+        self.e_0 = simulation_parameters["detector_properties"]["dark_count_error"]
+
     def channel_efficiency(self) -> float:
-        
-        t_ab = 10**(-1.0*self.beta*self.l/10.0)         #Channel transmittance
-        eta_bob = self.t_bob*self.eta_d                 #Receiver efficiency
-        
+        """
+        Computes the channel efficiency eta as:
+
+            eta = t_ab*eta_bob
+
+        Where the transmittance between a and b is given by:
+
+            t_ab = 10^(-beta*l/10)
+
+        Where beta is the loss coefficient of the channel and l is its lenght.
+        T receptor efficiency is calculated as:
+
+            eta_bob = t_bob*eta_d
+
+        With t_bob being the receptors optical circuit transmittance and eta_d the detector
+        efficiency.
+
+        Returns:
+            float: Channel Efficiency eta
+        """
+
+        t_ab = 10 ** (-1.0 * self.beta * self.l / 10.0)  # Channel transmittance
+        eta_bob = self.t_bob * self.eta_d  # Receiver efficiency
+
         if self.debug:
             print(f"[DEBUG] Channel efficiency {t_ab*eta_bob}")
-        
-        return t_ab*eta_bob
-    
-    def compute_detection_probabilities(self, eta: float, photon_nums: np.ndarray) -> np.ndarray:
-        
-        eta_n = 1 - (1 - eta)**photon_nums 
-        
-        p_none = (1 - (1 - self.e_d)*eta_n - (1 - self.e_0)*self.y_0)*(1 - self.e_d*eta_n - self.e_0*self.y_0) 
-        p_corr = ((1 - self.e_d)*eta_n + (1 - self.e_0)*self.y_0)*(1 - self.e_d*eta_n - self.e_0*self.y_0) 
-        p_errn = (1 - (1 - self.e_d)*eta_n - (1 - self.e_0)*self.y_0)*(self.e_d*eta_n + self.e_0*self.y_0)
-        p_both = ((1 - self.e_d)*eta_n + (1 - self.e_0)*self.y_0)*(self.e_d*eta_n + self.e_0*self.y_0)
-        
-        #probabilities are [N,1] arrays so they are joined on a [N,4] matrix:
-        
+
+        return t_ab * eta_bob
+
+    def compute_detection_probabilities(
+        self, eta: float, photon_nums: np.ndarray
+    ) -> np.ndarray:
+        """
+        Computes the probabilities of the 4 possible events for a two detector model:
+
+        p_none: Probability of no detection on either detector.
+        p_corr: Probability of the signal being detected completely by the corresponding
+        detector.
+        p_errn: Probability of the signal being detected completely by the erroneous detector.
+        p_both: Probability of the signal being detected by both detectors simultaneously.
+
+        Args:
+            eta (float): Channel efficiency
+            photon_nums (np.ndarray): Array containing the photon numbers of each pulse
+
+        Returns:
+            np.ndarray: A (N, 4) matrix containing the probabilities of each event in its columns
+            for each one of the pulses sent.
+        """
+        eta_n = 1 - (1 - eta) ** photon_nums
+
+        p_none = (1 - (1 - self.e_d) * eta_n - (1 - self.e_0) * self.y_0) * (
+            1 - self.e_d * eta_n - self.e_0 * self.y_0
+        )
+        p_corr = ((1 - self.e_d) * eta_n + (1 - self.e_0) * self.y_0) * (
+            1 - self.e_d * eta_n - self.e_0 * self.y_0
+        )
+        p_errn = (1 - (1 - self.e_d) * eta_n - (1 - self.e_0) * self.y_0) * (
+            self.e_d * eta_n + self.e_0 * self.y_0
+        )
+        p_both = ((1 - self.e_d) * eta_n + (1 - self.e_0) * self.y_0) * (
+            self.e_d * eta_n + self.e_0 * self.y_0
+        )
+
+        # probabilities are [N,1] arrays so they are joined on a [N,4] matrix:
+
         detection_probs = np.column_stack([p_none, p_corr, p_errn, p_both])
-        
+
         if self.debug:
             print(f"[DEBUG] Probability matrix: {detection_probs}")
-        
+
         return detection_probs
-    
+
     def detect_pulse(self, eta: float, photon_nums: np.ndarray) -> np.ndarray:
-        
+        """
+        Chooses a detection event based on the probability matrix for each pulse and returns
+        an array with the corresponding decision:
+
+            0: No detection (Signal lost)
+            1: Correct detection
+            2: Erroneous detection
+            3: Detection on both
+
+        Args:
+            eta (float): Channel efficiency
+            photon_nums (np.ndarray): Array containing the photon numbers of each pulse
+
+        Returns:
+            np.ndarray: Array containing 0, 1, 2 or 3 depending on the decision of the detection
+            event
+        """
+
         detection_probs = self.compute_detection_probabilities(eta, photon_nums)
-        
-        cumulative_sum = detection_probs.cumsum(axis=1) # Computes the Nx4 matrix with the cumulative sum of row elements.
-        
-        #We generate an auxiliary array of size Nx1 with random numbers between 0 and 1
-         
-        aux = rng.random(size = (len(detection_probs), 1))
-        detection_event = (aux < cumulative_sum).argmax(axis = 1)
-        
-        if self.debug: 
+
+        cumulative_sum = detection_probs.cumsum(
+            axis=1
+        )  # Computes the Nx4 matrix with the cumulative sum of row elements.
+
+        # We generate an auxiliary array of size Nx1 with random numbers between 0 and 1
+
+        aux = rng.random(size=(len(detection_probs), 1))
+        detection_event = (aux < cumulative_sum).argmax(axis=1)
+
+        if self.debug:
             print(f"[DEBUG] Cumulative sum matrix: {cumulative_sum}")
-            print(f"[DEBUG] Randomly generated vector: {aux}")
+            print(f"[DEBUG] Randomly choice vector: {aux}")
             print(f"[DEBUG] Choosen detection event: {detection_event}")
-        
+
         return detection_event
-    
+
     def generate_basis_seq(self) -> np.ndarray:
-        
-        basis_seq = self.rng.integers(0, 2, size = self.N) 
-        
+        """
+        Generates the random basis sequence for the receptor.
+
+        Returns:
+            basis_seq: Random array representing the base in which each bit will be encoded (0 = Rectilinear, 1 = Hadamard)
+        """
+        basis_seq = self.rng.integers(0, 2, size=self.N)
+
         return basis_seq
 
+
 simulation_parameters = {
-    "N"                 : 10,                       # Number of generated pulses
-    "mu"                : 1.0,                      # Signal intensity
-    "decoy_intensities" : [0.5, 0.0],               # Decoy intensities
-    "decoy_rate"        : 0.25,                     # Decoy probability
-    "channel_properties" : {                                   
-        "beta"                  : 0.3,              # Loss coefficient (dB/Km)
-        "l"                     : 0.5,              # Channel lenght (Km)
-        "channel_loss"          : 0.8               # Attenuation coefficient
-        },
-    "detector_properties": {
-        "receiver_transmit"     : 0.6,              # Receiver transmittance
-        "detector_efficiency"   : 0.47,             # Detector Efficiency
-        "detector_error"        : 0.3,              # Probability of a pulse measured in the correct basis to trigger the wrong detector
-        "dark_count_rate"       : 1e-6,             # Probability of dark counts
-        "dark_count_error"      : 0.5               # Probability of dark counts triggering the wrong detector
-        
+    "N": 100,  # Number of generated pulses
+    "mu": 1.0,  # Signal intensity
+    "decoy_intensities": [0.5, 0.0],  # Decoy intensities
+    "decoy_rate": 0.25,  # Decoy probability
+    "channel_properties": {
+        "beta": 0.3,  # Loss coefficient (dB/Km)
+        "l": 0.5,  # Channel lenght (Km)
+        "channel_loss": 0.8,  # Attenuation coefficient
     },
-    "debug": False
+    "detector_properties": {
+        "receiver_transmit": 0.6,  # Receiver transmittance
+        "detector_efficiency": 0.47,  # Detector Efficiency
+        "detector_error": 0.3,  # Probability of a pulse measured in the correct basis to trigger the wrong detector
+        "dark_count_rate": 1e-6,  # Probability of dark counts
+        "dark_count_error": 0.5,  # Probability of dark counts triggering the wrong detector
+    },
+    "debug": True,
 }
 rng = np.random.default_rng()
 alice = Source(simulation_parameters, rng)
@@ -99,7 +177,3 @@ bob = Detector(simulation_parameters, rng)
 eta = bob.channel_efficiency()
 
 p = bob.detect_pulse(eta, photon_nums)
-    
-  
-        
-        
--- source.py
+++ source.py
@@ -1,8 +1,16 @@
 import numpy as np
 
+
 class Source:
     def __init__(self, simulation_parameters: dict, rng: np.random.Generator):
-        
+        """
+        Initialize a photon source for the BB84 protocol
+
+        Args:
+            simulation_parameters (dict): Dictionary containing the simulation parameters
+            rng (np.random.Generator): Random number generator
+        """
+
         self.N = simulation_parameters["N"]
         self.mu = simulation_parameters["mu"]
         self.decoy_intensities = simulation_parameters["decoy_intensities"]
@@ -11,97 +19,100 @@ class Source:
         self.rng = rng
 
     def generate_bit_seq(self) -> np.ndarray:
-        
         """
         Generate a random bit sequence for the source (uses self.N and self.rng)
 
         Returns:
-            source_bits: Random array of representing the bits of the key 
+            source_bits: Random array of representing the bits of the key
         """
-        
+
         source_bits = self.rng.integers(0, 2, self.N)
-        
+
         if self.debug:
-            print(f'[DEBUG] Source bits: {source_bits}')
-            
+            print(f"[DEBUG] Source bits: {source_bits}")
+
         return source_bits
 
-    def generate_basis_seq(self) -> np.ndarray: 
+    def generate_basis_seq(self) -> np.ndarray:
         """
-        Generates the basis sequence in which the bits are to be encoded. 
+        Generates the basis sequence in which the bits are to be encoded.
 
         Returns:
             source_basis: Random array representing the base in which each bit will be encoded (0 = Rectilinear, 1 = Hadamard)
         """
-        
+
         source_basis = self.rng.integers(0, 2, self.N)
-        
+
         if self.debug:
-            print(f'[DEBUG] Source basis: {source_basis}')
-        
+            print(f"[DEBUG] Source basis: {source_basis}")
+
         return source_basis
 
     def generate_state_seq(self) -> np.ndarray:
         """
-        Assigns a number to the signal state (0) and for the decoy states (1, 2, ...) and generates the sequence of states.  
+        Assigns a number to the signal state (0) and for the decoy states (1, 2, ...) and generates the sequence of states.
 
         Returns:
-            decoy_sequence: Sequence of numbers that indicate the state of the pulse.  
-        
+            decoy_sequence: Sequence of numbers that indicate the state of the pulse.
+
         """
         decoy_num = int(len(self.decoy_intensities))
-        
-        state_index = [0] + list(np.arange(1, decoy_num + 1))
-        state_probs = [1 - self.decoy_rate] + [self.decoy_rate/decoy_num]*decoy_num  #Concatenation of arrays
 
-        state_sequence = self.rng.choice(state_index, size = self.N, p = state_probs)
-        
+        state_index = [0] + list(np.arange(1, decoy_num + 1))
+        state_probs = [1 - self.decoy_rate] + [
+            self.decoy_rate / decoy_num
+        ] * decoy_num  # Concatenation of arrays
+        state_probs = np.array(state_probs, dtype=np.float64) / np.sum(
+            state_probs
+        )  # Normalization of probabilities
+
+        state_sequence = self.rng.choice(state_index, size=self.N, p=state_probs)
+
         if self.debug:
-            print(f'[DEBUG] State choice: {state_sequence}')
+            print(f"[DEBUG] State choice: {state_sequence}")
 
         return state_sequence
 
     def generate_photon_number_seq(self, state_choice: np.ndarray) -> np.ndarray:
         """
-        Computes the number of photons in each pulse from a poissonian distribution with a mean intensity based on the choice of state (Decoy or signal) from decoy_choice. 
+        Computes the number of photons in each pulse from a poissonian distribution with a mean intensity based on the choice of state (Decoy or signal) from decoy_choice.
 
         Args:
             state_choice (np.ndarray): Array containing the index associated to pulse and decoy intensities
-   
+
         Returns:
-            photon_nums: Sequence with the number of photons sent for each pulse. 
-        """ 
-        intensities = [self.mu] + self.decoy_intensities #Intensity list
-        
-        photon_nums = np.zeros(self.N, dtype = int)
-        
+            photon_nums: Sequence with the number of photons sent for each pulse.
+        """
+        intensities = [self.mu] + self.decoy_intensities  # Intensity list
+
+        photon_nums = np.zeros(self.N, dtype=int)
+
         for i in range(len(intensities)):
             intensity_mask = state_choice == i
-            photon_nums[intensity_mask] = self.rng.poisson(intensities[i], size = np.sum(intensity_mask))
-        
-        
+            photon_nums[intensity_mask] = self.rng.poisson(
+                intensities[i], size=np.sum(intensity_mask)
+            )
+
         if self.debug:
-            print(f'[DEBUG] Photon numbers: {photon_nums}')
-        
+            print(f"[DEBUG] Photon numbers: {photon_nums}")
+
         return photon_nums
 
-    def generate_pulses(self) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: 
+    def generate_pulses(self) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
-        Returns a tuple with the bit sequence, the basis choice, the state choice and the photon number for each pulse. 
+        Returns a tuple with the bit sequence, the basis choice, the state choice and the photon number for each pulse.
 
         Args:
             simulation_parameters (dict): Dictionary containing the simulation parameters
             rng (np.random.Generator): Random number generator
 
         Returns:
-            tuple[source_bit_seq, source_basis_seq, source_state_seq, photon_nums]: Tuple with the information of the generated pulses. 
+            tuple[source_bit_seq, source_basis_seq, source_state_seq, photon_nums]: Tuple with the information of the generated pulses.
         """
-        
+
         source_bit_seq = self.generate_bit_seq()
         source_basis_seq = self.generate_basis_seq()
         source_state_seq = self.generate_state_seq()
         photon_nums = self.generate_photon_number_seq(source_state_seq)
-        
+
         return source_bit_seq, source_basis_seq, source_state_seq, photon_nums
-
-
